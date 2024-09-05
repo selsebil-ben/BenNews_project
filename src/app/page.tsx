68802e4c9562a542e345f4d09f3daf3235141e33
selsebil-ben <selsebilben.dev@gmail.com>
2024-09-05 11:49:54 +0200

    remove API key value in comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
  'use client';
-import Image from "next/image";
-import styles from "./page.module.css";
-import Link from "next/link";
+// import Image from "next/image";
+// import styles from "./page.module.css";
+// import Link from "next/link";
 import Navbar from "@/compo/Navbar";
 import NewsBoard from "@/compo/NewsBoard";
 import { useState } from "react";
